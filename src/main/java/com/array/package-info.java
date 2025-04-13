f09fcf6d440b557ce77b137b876c07a5dbcfbf2c
Mrunalini <dudhagawali@gmail.com>
2025-04-13 22:44:48 +0530

    more info added

--- src/main/java/com/array/package-info.java
+++ src/main/java/com/array/package-info.java
@@ -186,6 +186,7 @@ package com.array;
 
 /**
  * 152. Maximum Product Subarray
+ * Companies - Medium	Bloomberg
  * Given an integer array nums, find a subarray that has the largest product, and return the product.
  *
  * The test cases are generated so that the answer will fit in a 32-bit integer.
