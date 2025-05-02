caeaa66335732d31436ceb6280dea4c166c94809
Mrunalini <dudhagawali@gmail.com>
2025-05-02 21:16:03 +0530

    LC 22, 39, 46, 140, 231, 784, 1342 added

---
+++ src/main/java/com/array/Leetcode46.java
@@ -0,0 +1,35 @@
+package com.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode46 {
+    public static void main(String[] args) {
+        System.out.println(permute(new int[]{1,2,3}));
+    }
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean used[] = new boolean[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            used[i]=false;
+        }
+
+        permutations(0, nums, new ArrayList<>(), res, used);
+
+        return res;
+    }
+    public static void permutations(int ind, int[] nums, List<Integer> path, List<List<Integer>> res, boolean[] used)
+    {
+        if(used[ind]) return;
+
+        //include
+        used[ind] = true;
+        path.add(nums[ind]);
+        permutations(ind+1, nums, path, res, used);
+
+        // exclude
+        path.remove(nums[ind]);
+        used[ind] = false;
+    }
+}
